e3d444f6b8b0748c710d23e2f686465e3d2effbf
jeru <john.ungab7@gmail.com>
2025-03-04 18:35:01 +0800

    add: booking lists initial display

--- app/src/main/java/com/example/ungabhotel/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ungabhotel/ui/home/HomeFragment.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ungabhotel.databinding.FragmentHomeBinding;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private Button searchButton;
+    private ListView bookingListView;
     private DBHelper dbHelper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -25,10 +31,13 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-        dbHelper = new DBHelper(this);
+        dbHelper = new DBHelper(requireContext());
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // ---
+        InitializeComponents();
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
@@ -39,8 +48,24 @@ public class HomeFragment extends Fragment {
     {
         searchButton = binding.searchBtn;
         searchButton.setOnClickListener(view -> openSearchActivity());
+
+        bookingListView = binding.bookingLists;
+        DisplayBooking();
     }
 
+    // display the initial list of available booking
+    private void DisplayBooking()
+    {
+        List<String> availableBookings = dbHelper.getAvailableBooking();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                availableBookings
+        );
+        bookingListView.setAdapter(adapter);
+    }
+
+    // navigate to search activity
     private void openSearchActivity()
     {
         Intent intent = new Intent(getActivity(), SearchActivity.class);
