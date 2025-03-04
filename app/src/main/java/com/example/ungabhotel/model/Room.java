da2766888c0bd7370adba777eebeb5ce183de0d2
jeru <john.ungab7@gmail.com>
2025-03-04 19:55:14 +0800

    add: getter and setters

--- app/src/main/java/com/example/ungabhotel/model/Room.java
+++ app/src/main/java/com/example/ungabhotel/model/Room.java
@@ -3,4 +3,25 @@ package com.example.ungabhotel.model;
 public class Room {
     private int roomNumber;
     private boolean isAvail;
+
+    public Room(int roomNumber, boolean isAvail) {
+        this.roomNumber = roomNumber;
+        this.isAvail = isAvail;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public boolean isAvail() {
+        return isAvail;
+    }
+
+    public void setAvail(boolean avail) {
+        isAvail = avail;
+    }
 }
