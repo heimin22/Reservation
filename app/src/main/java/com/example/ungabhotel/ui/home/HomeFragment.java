676d7f3fe2213c5504dbb0d9e75ca8e70572f9c3
Sairusses <kurtarias123@gmail.com>
2025-03-04 21:09:29 +0800

    adjusted DisplayAvailableRooms

--- app/src/main/java/com/example/ungabhotel/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ungabhotel/ui/home/HomeFragment.java
@@ -65,6 +65,7 @@ public class HomeFragment extends Fragment {
         for(Room room : availableRooms)
         {
             roomStringify.add("Room: " + room.getRoomNumber());
+            roomStringify.add("Room type: " + room.getRoomType());
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
@@ -114,13 +115,13 @@ public class HomeFragment extends Fragment {
 //        });
 //    }
 
-//    // switch to booking details act  passing the selected booking id
-//    private void openBookingDetails(Booking booking)
-//    {
-//        Intent intent = new Intent(getActivity(), BookingDetailsActivity.class);
-//        intent.putExtra("BOOKING_ID", booking.getId());
-//        startActivity(intent);
-//    }
+    // switch to booking details act nd passing the selected booking id
+    private void openBookingDetails(Booking booking)
+    {
+        Intent intent = new Intent(getActivity(), BookingDetailsActivite.class);
+        intent.putExtra("BOOKING_ID", booking.getId());
+        startActivity(intent);
+    }
 
     // navigate to search activity
     private void openSearchActivity()
