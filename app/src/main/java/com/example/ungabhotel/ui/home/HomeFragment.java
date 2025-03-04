9706439440c208e55e9d22d9ed30d60d56ca16b0
jeru <john.ungab7@gmail.com>
2025-03-04 18:18:44 +0800

    add: search btn (home) and search activity

--- app/src/main/java/com/example/ungabhotel/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ungabhotel/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.ungabhotel.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,11 +17,15 @@ import com.example.ungabhotel.databinding.FragmentHomeBinding;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private Button searchButton;
+    private DBHelper dbHelper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
+
+        dbHelper = new DBHelper(this);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -27,6 +33,18 @@ public class HomeFragment extends Fragment {
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    private void InitializeComponents()
+    {
+        searchButton = binding.searchBtn;
+        searchButton.setOnClickListener(view -> openSearchActivity());
+    }
+
+    private void openSearchActivity()
+    {
+        Intent intent = new Intent(getActivity(), SearchActivity.class);
+        startActivity(intent);
     }
 
     @Override
